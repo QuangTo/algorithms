f26c39d4c00765a3c4e5684699c72bd60f2e1157
QuangTo <tvquangcs@gmail.com>
2024-10-09 09:31:09 +0700

    two pointers technique

--- array/findIndicesForSum.js
+++ array/findIndicesForSum.js
@@ -1,3 +1,14 @@
+// TWO POINTERS TECHNIQUE
+
+/**
+ * @param  {number[]} arr
+ * @param  {number} target
+ * @returns {number[]} resultArr
+ */
+/**
+ * 2 for, so the complex is 0(n^2), traditional
+ * */
+
 function findIndicesForSum(arr, target) {
   let resultArr = [];
   for (let i = 0; i < arr.length; i++) {
@@ -10,5 +21,55 @@ function findIndicesForSum(arr, target) {
   return resultArr;
 }
 
-const result = findIndicesForSum([0, 1, 2, 3, 4, 2, 5], 5);
-console.log(result);
+// console.log(findIndicesForSum([0, 1, 4, 5, 6, 8], 7));
+
+/**
+ * using two pointers
+ * */
+function twoPointers(arr, target) {
+  let result = [];
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === target) {
+      result.push([arr[left], arr[right]]);
+      return result;
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return result;
+}
+
+// console.log(twoPointers([0, 1, 4, 5, 6], 7));
+
+/**
+ * longer arr
+ */
+
+// arr = [ 2,4,1,5,7,3,3]
+// target = 6
+// output = 2-4 ,1-5, 3-3
+function twoPointersLongerArray(arr, target) {
+  let result = [];
+  let numMap = new Map();
+
+  for (let i = 0; i < arr.length; i++) {
+    const complement = target - arr[i];
+    console.log("complement", complement);
+
+    if (numMap.has(complement)) {
+      result.push([complement, arr[i]]);
+      console.log("push", complement, arr[i]);
+    }
+    numMap.set(arr[i], i);
+    console.log("set", arr[i], i);
+  }
+  return result;
+}
+
+console.log(twoPointersLongerArray([2, 4, 1, 5, 7, 3, 3], 6));
